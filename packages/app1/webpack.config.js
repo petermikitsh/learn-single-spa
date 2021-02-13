961a5a0c68ba7a1d80ae48548f7517080b220cf4
Peter Mikitsh <peter.mikitsh@gmail.com>
2021-02-13 12:53:59 -0800

    add app1

---
+++ packages/app1/webpack.config.js
@@ -0,0 +1,23 @@
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "exampleorg",
+    projectName: "app1",
+    webpackConfigEnv,
+    argv,
+  });
+
+  console.log(defaultConfig);
+
+  const final = {
+    ...defaultConfig,
+    devServer: {
+      ...defaultConfig.devServer,
+      port: "8000",
+    },
+    externals: [/^@exampleorg\//],
+  };
+
+  return final;
+};
