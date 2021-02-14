da9321e521a89bab790c4bedcd85546330847fe5
Peter Mikitsh <peter.mikitsh@gmail.com>
2021-02-13 21:47:46 -0800

    chore: fix gh-pages cli args

--- packages/app2/scripts/version.sh
+++ packages/app2/scripts/version.sh
@@ -11,4 +11,7 @@ SUBPACKAGE_DIR="$(dirname $( cd "$BASE_DIR" ; pwd -P ))"
 CURR_VERSION=$(node -p "require('${SUBPACKAGE_DIR}/package.json').version")
 SRC_GLOB=$SUBPACKAGE_DIR/dist/\!\(*.html\)
 
-npx --no-install gh-pages --add --src $SRC_GLOB --dest @exampleorg/app2/${CURR_VERSION}
+yarn run gh-pages --add \
+  --dist $SUBPACKAGE_DIR/dist \
+  --src "**/!(*.html)" \
+  --dest @exampleorg/app2/${CURR_VERSION}
