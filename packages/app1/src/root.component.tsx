beca5bfe27ba49df27576d3ac9c9c30b029c3529
Peter Mikitsh <peter.mikitsh@gmail.com>
2021-02-14 21:12:57 -0800

    chore: use util4 in app1

--- packages/app1/src/root.component.tsx
+++ packages/app1/src/root.component.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const [sum, setSum] = React.useState(null);
+  React.useEffect(() => {
+    (async () => {
+      // add is type 'any'
+      const { add } = await System.import("@exampleorg/util4");
+      setSum(add(2, 2));
+    })();
+  });
+  return (
+    <>
+      <section>{props.name} is mounted!</section>
+      <div>Sum is {sum}.</div>
+    </>
+  );
 }
