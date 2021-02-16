1dfe4dcf937634397dc7c7739e7dc89fb808f2ff
Peter Mikitsh <peter.mikitsh@gmail.com>
2021-02-16 10:16:25 -0800

    fix: add script to publish util4

---
+++ packages/util4/scripts/version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Log Each command
+set -x
+
+# Bail on first error
+set -e
+
+BASE_DIR=$(dirname $0)
+SUBPACKAGE_DIR="$(dirname $( cd "$BASE_DIR" ; pwd -P ))"
+CURR_VERSION=$(node -p "require('${SUBPACKAGE_DIR}/package.json').version")
+
+npx --no-install gh-pages --add \
+  --dist $SUBPACKAGE_DIR/dist \
+  --src "**/!(*.html)" \
+  --dest @exampleorg/util4/${CURR_VERSION}
